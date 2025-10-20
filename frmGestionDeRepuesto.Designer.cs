8445fbb3178871ac5518da81d2eaf9bd8982d938
Martina Roldan <laramartinaroldan@gmail.com>
2025-10-20 19:41:54 -0300

    Agregado De Datos

--- frmGestionDeRepuesto.Designer.cs
+++ frmGestionDeRepuesto.Designer.cs
@@ -29,25 +29,25 @@
         private void InitializeComponent()
         {
             grbIngreso = new GroupBox();
-            lblMarca = new Label();
-            lblNumeroRepuesto = new Label();
-            lstMarca = new ComboBox();
-            mtbRepuestos = new MaskedTextBox();
-            lblDescripcion = new Label();
-            txtDescripcion = new TextBox();
-            lblPrecio = new Label();
-            mtbPrecio = new MaskedTextBox();
-            lblOrigen = new Label();
-            lstOrigen = new ComboBox();
             cmdAgregarRepuesto = new Button();
+            lstOrigen = new ComboBox();
+            lblOrigen = new Label();
+            mtbPrecio = new MaskedTextBox();
+            lblPrecio = new Label();
+            txtDescripcion = new TextBox();
+            lblDescripcion = new Label();
+            mtbRepuestos = new MaskedTextBox();
+            lstMarca = new ComboBox();
+            lblNumeroRepuesto = new Label();
+            lblMarca = new Label();
             groupBox1 = new GroupBox();
-            lblMarca2 = new Label();
-            comboBox1 = new ComboBox();
-            lblOrigen2 = new Label();
-            optNacional = new RadioButton();
-            radioButton1 = new RadioButton();
-            ltbResultado = new ListBox();
             cmdConsultar = new Button();
+            ltbResultado = new ListBox();
+            optImportado = new RadioButton();
+            optNacional = new RadioButton();
+            lblOrigen2 = new Label();
+            lstMarca2 = new ComboBox();
+            lblMarca2 = new Label();
             grbIngreso.SuspendLayout();
             groupBox1.SuspendLayout();
             SuspendLayout();
@@ -72,6 +72,96 @@
             grbIngreso.TabIndex = 0;
             grbIngreso.TabStop = false;
             grbIngreso.Text = "Ingreso De Repuestos ";
+          
+            // 
+            // cmdAgregarRepuesto
+            // 
+            cmdAgregarRepuesto.Location = new Point(391, 149);
+            cmdAgregarRepuesto.Name = "cmdAgregarRepuesto";
+            cmdAgregarRepuesto.Size = new Size(195, 29);
+            cmdAgregarRepuesto.TabIndex = 5;
+            cmdAgregarRepuesto.Text = "Agregar Repuesto";
+            cmdAgregarRepuesto.UseVisualStyleBackColor = true;
+            cmdAgregarRepuesto.Click += cmdAgregarRepuesto_Click;
+            // 
+            // lstOrigen
+            // 
+            lstOrigen.FormattingEnabled = true;
+            lstOrigen.Items.AddRange(new object[] { "Nacional ", "Importado " });
+            lstOrigen.Location = new Point(435, 64);
+            lstOrigen.Name = "lstOrigen";
+            lstOrigen.Size = new Size(151, 28);
+            lstOrigen.TabIndex = 4;
+            // 
+            // lblOrigen
+            // 
+            lblOrigen.AutoSize = true;
+            lblOrigen.Location = new Point(357, 66);
+            lblOrigen.Name = "lblOrigen";
+            lblOrigen.Size = new Size(72, 20);
+            lblOrigen.TabIndex = 1;
+            lblOrigen.Text = "Origen ";
+            // 
+            // mtbPrecio
+            // 
+            mtbPrecio.Location = new Point(146, 197);
+            mtbPrecio.Mask = "99999";
+            mtbPrecio.Name = "mtbPrecio";
+            mtbPrecio.Size = new Size(151, 28);
+            mtbPrecio.TabIndex = 3;
+            mtbPrecio.ValidatingType = typeof(int);
+            // 
+            // lblPrecio
+            // 
+            lblPrecio.AutoSize = true;
+            lblPrecio.Location = new Point(65, 197);
+            lblPrecio.Name = "lblPrecio";
+            lblPrecio.Size = new Size(60, 20);
+            lblPrecio.TabIndex = 1;
+            lblPrecio.Text = "Precio";
+            // 
+            // txtDescripcion
+            // 
+            txtDescripcion.Location = new Point(146, 139);
+            txtDescripcion.MaxLength = 50;
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(151, 28);
+            txtDescripcion.TabIndex = 2;
+            // 
+            // lblDescripcion
+            // 
+            lblDescripcion.AutoSize = true;
+            lblDescripcion.Location = new Point(17, 139);
+            lblDescripcion.Name = "lblDescripcion";
+            lblDescripcion.Size = new Size(108, 20);
+            lblDescripcion.TabIndex = 4;
+            lblDescripcion.Text = "Descripción";
+            // 
+            // mtbRepuestos
+            // 
+            mtbRepuestos.Location = new Point(146, 87);
+            mtbRepuestos.Mask = "999999";
+            mtbRepuestos.Name = "mtbRepuestos";
+            mtbRepuestos.Size = new Size(151, 28);
+            mtbRepuestos.TabIndex = 1;
+            // 
+            // lstMarca
+            // 
+            lstMarca.FormattingEnabled = true;
+            lstMarca.Items.AddRange(new object[] { "Peugeot (P)", "Fiat (F)", "Renault (R)" });
+            lstMarca.Location = new Point(146, 43);
+            lstMarca.Name = "lstMarca";
+            lstMarca.Size = new Size(151, 28);
+            lstMarca.TabIndex = 0;
+            // 
+            // lblNumeroRepuesto
+            // 
+            lblNumeroRepuesto.AutoSize = true;
+            lblNumeroRepuesto.Location = new Point(17, 90);
+            lblNumeroRepuesto.Name = "lblNumeroRepuesto";
+            lblNumeroRepuesto.Size = new Size(111, 20);
+            lblNumeroRepuesto.TabIndex = 1;
+            lblNumeroRepuesto.Text = "N°Repuesto";
             // 
             // lblMarca
             // 
@@ -83,102 +173,14 @@
             lblMarca.TabIndex = 1;
             lblMarca.Text = "Marca";
             // 
-            // lblNumeroRepuesto
-            // 
-            lblNumeroRepuesto.AutoSize = true;
-            lblNumeroRepuesto.Location = new Point(17, 90);
-            lblNumeroRepuesto.Name = "lblNumeroRepuesto";
-            lblNumeroRepuesto.Size = new Size(111, 20);
-            lblNumeroRepuesto.TabIndex = 1;
-            lblNumeroRepuesto.Text = "N°Repuesto";
-            // 
-            // lstMarca
-            // 
-            lstMarca.FormattingEnabled = true;
-            lstMarca.Items.AddRange(new object[] { "Peugeot (P)", "Fiat (F)", "Renault (R)" });
-            lstMarca.Location = new Point(146, 43);
-            lstMarca.Name = "lstMarca";
-            lstMarca.Size = new Size(151, 28);
-            lstMarca.TabIndex = 2;
-            // 
-            // mtbRepuestos
-            // 
-            mtbRepuestos.Location = new Point(146, 87);
-            mtbRepuestos.Mask = "999999";
-            mtbRepuestos.Name = "mtbRepuestos";
-            mtbRepuestos.Size = new Size(151, 28);
-            mtbRepuestos.TabIndex = 3;
-            // 
-            // lblDescripcion
-            // 
-            lblDescripcion.AutoSize = true;
-            lblDescripcion.Location = new Point(17, 139);
-            lblDescripcion.Name = "lblDescripcion";
-            lblDescripcion.Size = new Size(108, 20);
-            lblDescripcion.TabIndex = 4;
-            lblDescripcion.Text = "Descripción";
-            // 
-            // txtDescripcion
-            // 
-            txtDescripcion.Location = new Point(146, 139);
-            txtDescripcion.MaxLength = 50;
-            txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(151, 28);
-            txtDescripcion.TabIndex = 5;
-            // 
-            // lblPrecio
-            // 
-            lblPrecio.AutoSize = true;
-            lblPrecio.Location = new Point(65, 197);
-            lblPrecio.Name = "lblPrecio";
-            lblPrecio.Size = new Size(60, 20);
-            lblPrecio.TabIndex = 1;
-            lblPrecio.Text = "Precio";
-            // 
-            // mtbPrecio
-            // 
-            mtbPrecio.Location = new Point(146, 197);
-            mtbPrecio.Mask = "99999";
-            mtbPrecio.Name = "mtbPrecio";
-            mtbPrecio.Size = new Size(151, 28);
-            mtbPrecio.TabIndex = 6;
-            mtbPrecio.ValidatingType = typeof(int);
-            // 
-            // lblOrigen
-            // 
-            lblOrigen.AutoSize = true;
-            lblOrigen.Location = new Point(357, 66);
-            lblOrigen.Name = "lblOrigen";
-            lblOrigen.Size = new Size(72, 20);
-            lblOrigen.TabIndex = 1;
-            lblOrigen.Text = "Origen ";
-            // 
-            // lstOrigen
-            // 
-            lstOrigen.FormattingEnabled = true;
-            lstOrigen.Items.AddRange(new object[] { "Nacional ", "Importado " });
-            lstOrigen.Location = new Point(435, 64);
-            lstOrigen.Name = "lstOrigen";
-            lstOrigen.Size = new Size(151, 28);
-            lstOrigen.TabIndex = 1;
-            // 
-            // cmdAgregarRepuesto
-            // 
-            cmdAgregarRepuesto.Location = new Point(391, 149);
-            cmdAgregarRepuesto.Name = "cmdAgregarRepuesto";
-            cmdAgregarRepuesto.Size = new Size(195, 29);
-            cmdAgregarRepuesto.TabIndex = 7;
-            cmdAgregarRepuesto.Text = "Agregar Repuesto";
-            cmdAgregarRepuesto.UseVisualStyleBackColor = true;
-            // 
             // groupBox1
             // 
             groupBox1.Controls.Add(cmdConsultar);
             groupBox1.Controls.Add(ltbResultado);
-            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(optImportado);
             groupBox1.Controls.Add(optNacional);
             groupBox1.Controls.Add(lblOrigen2);
-            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(lstMarca2);
             groupBox1.Controls.Add(lblMarca2);
             groupBox1.Location = new Point(28, 310);
             groupBox1.Name = "groupBox1";
@@ -187,24 +189,48 @@
             groupBox1.TabStop = false;
             groupBox1.Text = "Consulta De Repuesto";
             // 
-            // lblMarca2
+            // cmdConsultar
             // 
-            lblMarca2.AutoSize = true;
-            lblMarca2.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            lblMarca2.Location = new Point(17, 45);
-            lblMarca2.Name = "lblMarca2";
-            lblMarca2.Size = new Size(59, 20);
-            lblMarca2.TabIndex = 0;
-            lblMarca2.Text = "Marca";
+            cmdConsultar.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmdConsultar.Location = new Point(69, 139);
+            cmdConsultar.Name = "cmdConsultar";
+            cmdConsultar.Size = new Size(135, 29);
+            cmdConsultar.TabIndex = 2;
+            cmdConsultar.Text = "Consultar";
+            cmdConsultar.UseVisualStyleBackColor = true;
+            cmdConsultar.Click += cmdConsultar_Click;
             // 
-            // comboBox1
+            // ltbResultado
             // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] { "P", "F", "R" });
-            comboBox1.Location = new Point(82, 43);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(151, 28);
-            comboBox1.TabIndex = 1;
+            ltbResultado.FormattingEnabled = true;
+            ltbResultado.Location = new Point(357, 43);
+            ltbResultado.Name = "ltbResultado";
+            ltbResultado.Size = new Size(359, 124);
+            ltbResultado.TabIndex = 3;
+            // 
+            // optImportado
+            // 
+            optImportado.AutoSize = true;
+            optImportado.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            optImportado.Location = new Point(210, 98);
+            optImportado.Name = "optImportado";
+            optImportado.Size = new Size(127, 24);
+            optImportado.TabIndex = 4;
+            optImportado.TabStop = true;
+            optImportado.Text = "Importado ";
+            optImportado.UseVisualStyleBackColor = true;
+            // 
+            // optNacional
+            // 
+            optNacional.AutoSize = true;
+            optNacional.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            optNacional.Location = new Point(95, 98);
+            optNacional.Name = "optNacional";
+            optNacional.Size = new Size(109, 24);
+            optNacional.TabIndex = 1;
+            optNacional.TabStop = true;
+            optNacional.Text = "Nacional ";
+            optNacional.UseVisualStyleBackColor = true;
             // 
             // lblOrigen2
             // 
@@ -216,47 +242,24 @@
             lblOrigen2.TabIndex = 2;
             lblOrigen2.Text = "Origen ";
             // 
-            // optNacional
+            // lstMarca2
             // 
-            optNacional.AutoSize = true;
-            optNacional.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            optNacional.Location = new Point(95, 98);
-            optNacional.Name = "optNacional";
-            optNacional.Size = new Size(109, 24);
-            optNacional.TabIndex = 3;
-            optNacional.TabStop = true;
-            optNacional.Text = "Nacional ";
-            optNacional.UseVisualStyleBackColor = true;
+            lstMarca2.FormattingEnabled = true;
+            lstMarca2.Items.AddRange(new object[] { "P", "F", "R" });
+            lstMarca2.Location = new Point(82, 43);
+            lstMarca2.Name = "lstMarca2";
+            lstMarca2.Size = new Size(151, 28);
+            lstMarca2.TabIndex = 0;
             // 
-            // radioButton1
+            // lblMarca2
             // 
-            radioButton1.AutoSize = true;
-            radioButton1.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            radioButton1.Location = new Point(210, 98);
-            radioButton1.Name = "radioButton1";
-            radioButton1.Size = new Size(127, 24);
-            radioButton1.TabIndex = 4;
-            radioButton1.TabStop = true;
-            radioButton1.Text = "Importado ";
-            radioButton1.UseVisualStyleBackColor = true;
-            // 
-            // ltbResultado
-            // 
-            ltbResultado.FormattingEnabled = true;
-            ltbResultado.Location = new Point(418, 43);
-            ltbResultado.Name = "ltbResultado";
-            ltbResultado.Size = new Size(267, 104);
-            ltbResultado.TabIndex = 5;
-            // 
-            // cmdConsultar
-            // 
-            cmdConsultar.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            cmdConsultar.Location = new Point(69, 139);
-            cmdConsultar.Name = "cmdConsultar";
-            cmdConsultar.Size = new Size(135, 29);
-            cmdConsultar.TabIndex = 6;
-            cmdConsultar.Text = "Consultar";
-            cmdConsultar.UseVisualStyleBackColor = true;
+            lblMarca2.AutoSize = true;
+            lblMarca2.Font = new Font("Verdana", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            lblMarca2.Location = new Point(17, 45);
+            lblMarca2.Name = "lblMarca2";
+            lblMarca2.Size = new Size(59, 20);
+            lblMarca2.TabIndex = 0;
+            lblMarca2.Text = "Marca";
             // 
             // frmGestionDeRepuesto
             // 
@@ -267,6 +270,7 @@
             Controls.Add(grbIngreso);
             Name = "frmGestionDeRepuesto";
             Text = "Gestion De Repuestos ";
+            Load += frmGestionDeRepuesto_Load;
             grbIngreso.ResumeLayout(false);
             grbIngreso.PerformLayout();
             groupBox1.ResumeLayout(false);
@@ -290,11 +294,11 @@
         private Button cmdAgregarRepuesto;
         private GroupBox groupBox1;
         private Label lblOrigen2;
-        private ComboBox comboBox1;
+        private ComboBox lstMarca2;
         private Label lblMarca2;
         private Button cmdConsultar;
         private ListBox ltbResultado;
-        private RadioButton radioButton1;
+        private RadioButton optImportado;
         private RadioButton optNacional;
     }
 }
--- frmGestionDeRepuesto.cs
+++ frmGestionDeRepuesto.cs
@@ -6,5 +6,153 @@ namespace pryRoldanGestionDeRepuestos
         {
             InitializeComponent();
         }
+        string[,] repuestos = new string[100, 5];
+        int contador = 0;
+
+        private void frmGestionDeRepuesto_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdAgregarRepuesto_Click(object sender, EventArgs e)
+        {
+            if (contador >= 100)
+            {
+                MessageBox.Show("Ya se ingresaron los 100 repuestos permitidos.");
+                return;
+            }
+
+            if (lstMarca.Text == "")
+            {
+                MessageBox.Show("Debe seleccionar una marca.");
+                return;
+            }
+
+            if (mtbRepuestos.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un número de repuesto.");
+                return;
+            }
+
+
+            if (txtDescripcion.Text == "")
+            {
+                MessageBox.Show("Debe ingresar una descripción.");
+                return;
+            }
+            if (mtbPrecio.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un precio.");
+                return;
+            }
+
+            if (lstOrigen.Text == "")
+            {
+                MessageBox.Show("Debe seleccionar un origen.");
+                return;
+            }
+
+            string numero = mtbRepuestos.Text;
+            int num;
+
+            try
+            {
+                num = int.Parse(numero);
+                if (numero.Length > 6)
+                {
+                    MessageBox.Show("El número de repuesto no puede tener más de 6 dígitos.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ingrese un número de repuesto válido (solo números).");
+                return;
+            }
+
+            // Validar repetido
+            for (int i = 0; i < contador; i++)
+            {
+                if (repuestos[i, 2] == numero)
+                {
+                    MessageBox.Show("Ya existe un repuesto con ese número.");
+                    return;
+                }
+            }
+
+            string precio = mtbPrecio.Text;
+            float prec;
+
+            try
+            {
+                prec = float.Parse(precio);
+                if (prec <= 0)
+                {
+                    MessageBox.Show("Ingrese un precio mayor a 0.");
+                    return;
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Ingrese un precio válido.");
+                return;
+            }
+
+            // Carga en matriz
+            repuestos[contador, 0] = lstMarca.Text;
+            repuestos[contador, 1] = lstOrigen.Text;
+            repuestos[contador, 2] = numero;
+            repuestos[contador, 3] = txtDescripcion.Text;
+            repuestos[contador, 4] = precio;
+            contador++;
+
+            MessageBox.Show("Repuesto cargado correctamente.");
+
+            // Limpiar
+            lstMarca.SelectedIndex = -1;
+            mtbRepuestos.Text = "";
+            txtDescripcion.Text = "";
+            mtbPrecio.Text = "";
+
+        }
+
+        private void cmdConsultar_Click(object sender, EventArgs e)
+        {
+            if (contador == 0)
+            {
+                MessageBox.Show("No hay repuestos cargados.");
+                return;
+            }
+
+            if (lstMarca2.Text == "")
+            {
+                MessageBox.Show("Debe seleccionar una marca para consultar.");
+                return;
+            }
+
+            char marca = lstMarca2.Text[0];
+            char origen = optNacional.Checked ? 'N' : 'I';
+            bool encontrado = false;
+
+            ltbResultado.Items.Clear();
+
+            for (int i = 0; i < contador; i++)
+            {
+                if (repuestos[i, 0][0] == marca && repuestos[i, 1][0] == origen)
+                {
+                    encontrado = true;
+                    ltbResultado.Items.Add(
+                        "Número: " + repuestos[i, 2] +
+                        " | Descripción: " + repuestos[i, 3] +
+                        " | Precio: $" + repuestos[i, 4]
+                    );
+                }
+            }
+
+            if (!encontrado)
+            {
+                ltbResultado.Items.Add("No se encontraron repuestos con esos criterios.");
+            }
+        }
     }
 }
